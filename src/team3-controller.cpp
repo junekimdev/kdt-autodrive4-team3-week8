d4ec33f84cd01be3b14895d047cf038159c66ab0
June Kim <godlyjune@gmail.com>
2022-12-21 13:56:58 +0900

    Adopt states

--- src/team3-controller.cpp
+++ src/team3-controller.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <memory>
 #include <string>
 #include <vector>
@@ -11,8 +12,9 @@
 // #include "sensor_imu/imu_msg.h"
 #include "xycar_msgs/xycar_motor.h"
 
-// Include Drivers
-#include "../include/team3-controller/ControlDriver.h"
+// Include States
+#include "team3-controller/ControlState.h"
+#include "team3-controller/SensorState.h"
 
 // Const
 const std::string NODE_NAME = "controller";
@@ -29,28 +31,35 @@ class Controller {
   // ros::Subscriber sub_lidar;
   // ros::Subscriber sub_sonic;
   // ros::Subscriber sub_imu;
-  ros::Publisher pub;
-  int width;
-  int lpos;
-  int rpos;
+  SensorState& sensorState;
+  ControlState& controlState;
+
+  void correctAngle() {
+    this->controlState.angle += ANGLE_CENTER;
+    if (this->controlState.angle > MAX_ANGLE)
+      this->controlState.angle = MAX_ANGLE;
+    if (this->controlState.angle < MIN_ANGLE)
+      this->controlState.angle = MIN_ANGLE;
+  }
 
 public:
-  Controller() {
-    sub_cam = node.subscribe(SUB_TOPIC_CAM, 1, &Controller::callbackCam, this);
-    // sub_lidar = node.subscribe(SUB_TOPIC_LIDAR, 1,
+  Controller() : sensorState(SensorState()) {
+    this->sub_cam =
+        this->node.subscribe(SUB_TOPIC_CAM, 1, &Controller::callbackCam, this);
+    // this->sub_lidar = node.subscribe(SUB_TOPIC_LIDAR, 1,
     // &Controller::callbackLidar, this);
-    pub = node.advertise<xycar_msgs::xycar_motor>(PUB_TOPIC, 1);
+    this->controlState = ControlState(
+        this->node.advertise<xycar_msgs::xycar_motor>(PUB_TOPIC, 1));
   }
 
   void callbackCam(const sensor_cam::cam_msg::ConstPtr& msg);
   // void callbackLidar(const sensor_lidar::lidar_msg::ConstPtr& msg);
   void control();
+  void start();
 };
 
 void Controller::callbackCam(const sensor_cam::cam_msg::ConstPtr& msg) {
-  this->width = msg->width;
-  this->lpos = msg->lpos;
-  this->rpos = msg->rpos;
+  this->sensorState.reduceCamState(msg);
   this->control();
 }
 
@@ -59,23 +68,21 @@ void Controller::callbackCam(const sensor_cam::cam_msg::ConstPtr& msg) {
 //   ROS_INFO("CON:: %d", msg->size)
 // }
 
+// TODO:
 void Controller::control() {
-  // TODO:
-  int viewCenter = this->width / 2;
-  int laneCenter = (this->rpos + this->lpos) / 2;
-  int ang = laneCenter - viewCenter;
-  std::unique_ptr<ControlDriver> driver;
-  if (ang < 5) {
-    driver = std::make_unique<DriverGoSlow>();
-  } else if (ang > 50) {
-    driver = std::make_unique<DriverTurnSlow>(50);
-  } else if (ang < -50) {
-    driver = std::make_unique<DriverTurnSlow>(-50);
-  } else {
-    driver = std::make_unique<DriverTurnSlow>(ang);
-  }
-  driver->drive(this->pub);
+  // Decide angle
+  float viewCenter = sensorState.width / 2.f;
+  float laneCenter = (sensorState.lposSMA + sensorState.rposSMA) / 2.f;
+  int angle = (int)((laneCenter - viewCenter) / 3.f + .5f);  // Round half up
+  angle = correctAngle(angle);
+  DRIVE_MODE mode = (std::abs(angle) < 5) DRIVE.GO_SLOW : DRIVE.TURN_SLOW;
+  int speed = mode == DRIVE.GO_SLOW || mode == DRIVE.TURN_SLOW ? 10 : 30;
+
+  if (this->controlState.isStarted)
+    this->controlState.reduce(mode, angle, speed);
 }
+
+void Controller::start() { this->controlState.isStarted = true; }
 
 int main(int argc, char** argv) {
   // Init
@@ -86,6 +93,7 @@ int main(int argc, char** argv) {
   // Set repeat freq
   ros::Rate rate(FREQ);
 
+  controller.start();
   while (ros::ok()) {
     ros::spinOnce();
     rate.sleep();
