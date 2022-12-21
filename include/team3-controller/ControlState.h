ed7075d3fc45399ac7b62361249e71e3ac61d6a9
June Kim <godlyjune@gmail.com>
2022-12-21 16:10:41 +0900

    Combine SensorState file with ControlState

--- include/team3-controller/ControlState.h
+++ include/team3-controller/ControlState.h
@@ -1,7 +1,12 @@
 #ifndef CONTROL_STATE_H
 #define CONTROL_STATE_H
 
+#include <numeric>
+#include <vector>
+
+#include "ros/console.h"
 #include "ros/ros.h"
+#include "sensor_cam/cam_msg.h"
 #include "xycar_msgs/xycar_motor.h"
 
 // Const
@@ -11,46 +16,73 @@ constexpr int ANGLE_CENTER = 8;
 constexpr int MAX_ANGLE = 50;
 constexpr int MIN_ANGLE = -50;
 constexpr int MAX_SPEED = 30;
+constexpr int WIDTH = 640;
+constexpr int SMA_NUM = 3;
 
 enum struct DRIVE_MODE { STOP, GO_SLOW, GO_FAST, TURN_SLOW };
 
 struct ControlState {
-  ros::Publisher& pub;
   DRIVE_MODE mode;
   int angle;
   int speed;
   bool isStarted;
 
   ControlState()
-      : pub(nullptr),
-        mode(DRIVE.STOP),
+      : mode(DRIVE_MODE::STOP),
         angle(ANGLE_CENTER),
         speed(0),
         isStarted(false) {}
-  ControlState(ros::Publisher& pub)
-      : pub(pub),
-        mode(DRIVE.STOP),
-        angle(ANGLE_CENTER),
-        speed(0),
-        isStarted(false) {}
-
-  xycar_msgs::xycar_motor createMsg() {
-    xycar_msgs::xycar_motor msg;
-    msg.header.stamp = ros::Time::now();
-    msg.header.frame_id = CONTROLLER_NAME;
-    msg.angle = this->angle;
-    msg.speed = this->speed;
-    return msg;
-  }
-
-  void drive(ros::Publisher& pub) const { pub.publish(this->createMsg()); }
 
   void reduce(DRIVE_MODE mode, int angle, int speed) {
     this->mode = mode;
     this->angle = angle;
     this->speed = speed;
-    this->drive();
   }
 };
+
+struct SensorState {
+  int width;
+  int lpos;
+  int rpos;
+  std::vector<int> lposMemo;
+  std::vector<int> rposMemo;
+  int lposSMA;
+  int rposSMA;
+
+  SensorState()
+      : width(WIDTH),
+        lpos(0),
+        rpos(WIDTH - 1),
+        lposMemo(std::vector<int>(SMA_NUM, 0)),
+        rposMemo(std::vector<int>(SMA_NUM, WIDTH - 1)),
+        lposSMA(0),
+        rposSMA(WIDTH - 1) {}
+
+  void reduceCamState(const sensor_cam::cam_msg::ConstPtr& msg);
+  void updateCamState(const sensor_cam::cam_msg::ConstPtr& msg);
+  void filterCamState();
+};
+
+void SensorState::reduceCamState(const sensor_cam::cam_msg::ConstPtr& msg) {
+  this->updateCamState(msg);
+  this->filterCamState();
+}
+void SensorState::updateCamState(const sensor_cam::cam_msg::ConstPtr& msg) {
+  this->width = msg->width;
+  if (msg->isLeftDetected) this->lpos = msg->lpos;
+  if (msg->isRightDetected) this->rpos = msg->rpos;
+}
+void SensorState::filterCamState() {
+  lposMemo.erase(lposMemo.begin());
+  rposMemo.erase(rposMemo.begin());
+  lposMemo.emplace_back(this->lpos);
+  rposMemo.emplace_back(this->rpos);
+  this->lposSMA = (int)(std::accumulate(lposMemo.begin(), lposMemo.end(), 0) /
+                            (float)SMA_NUM +
+                        .5f);
+  this->rposSMA = (int)(std::accumulate(rposMemo.begin(), rposMemo.end(), 0) /
+                            (float)SMA_NUM +
+                        .5f);
+}
 
 #endif
